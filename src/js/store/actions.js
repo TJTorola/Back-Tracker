60dda5bd68e99877f96436da1b7aa8a836f3a759
Tyler Torola <tyler@tjt.codes>
2018-03-31 16:48:15 -0700

    Extract requestRoutes thunk

--- src/js/store/actions.js
+++ src/js/store/actions.js
@@ -2,15 +2,20 @@ import Bart from "~/api/bart";
 import { createThunk, createAction } from "~/util";
 
 export const receiveRoutes = createAction("RECEIVE_ROUTES");
-export const requestRoutes = createAction("REQUEST_ROUTES");
 export const setStatus = createAction("SET_STATUS");
 
 export const initialize = createThunk(
   "INITIALIZE_BACK_TRACKER",
   () => async ({ getState, dispatch }) => {
-    dispatch(requestRoutes());
+    await dispatch(requestRoutes());
+    dispatch(setStatus("LOADED"));
+  }
+);
+
+export const requestRoutes = createThunk(
+  "REQUEST_ROUTES",
+  () => async ({ dispatch }) => {
     const { routes } = await Bart.routes();
     dispatch(receiveRoutes(routes));
-    dispatch(setStatus("LOADED"));
   }
 );
