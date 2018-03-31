f1bc4573d29733f4bcbc1979d2f0e2fd98de06b5
Tyler Torola <tyler@tjt.codes>
2018-03-30 21:51:03 -0700

    Refine the generateUrl function

--- src/js/api/bart.js
+++ src/js/api/bart.js
@@ -1,16 +1,19 @@
 // DOCS: http://api.bart.gov/docs/overview/index.aspx
 
 // This is the public token
-const API_TOKEN = "MW9S-E7SL-26DU-VV8V";
-const BASE_URL = `http://api.bart.gov/api/route.aspx?key=${API_TOKEN}&json=y`;
+const BASE_URL = `http://api.bart.gov/api/`;
+const UNIVERSAL_PARAMS = {
+  key: "MW9S-E7SL-26DU-VV8V",
+  json: "y"
+};
 
-const generateUrl = (base, command, params = {}) => {
+const generateUrl = (base, command, addedParams = {}) => {
+  const params = Object.assign({}, UNIVERSAL_PARAMS, addedParams);
   const paramString = Object.keys(params)
     .map(p => `${p}=${params[p]}`)
     .join("&");
-  const andParams = paramString ? `&${paramString}` : "";
 
-  return `http://api.bart.gov/api/${base}.aspx?key=${API_TOKEN}&json=y&cmd=${command}${andParams}`;
+  return `http://api.bart.gov/api/${base}.aspx?cmd=${command}&${paramString}`;
 };
 
 const Bart = {
