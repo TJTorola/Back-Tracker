1b03f8c3a741b6e8b6144eb60b97e0aad85dd0ac
Tyler Torola <tyler@tjt.codes>
2018-03-30 21:46:31 -0700

    Attach to window for testing

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Bart from "~/api/bart";
 import App from "~/app";
 
 export const main = () => {
   // Mount the application into #app
+  window.Bart = Bart;
   ReactDOM.render(<App />, document.getElementById("app"));
 };
