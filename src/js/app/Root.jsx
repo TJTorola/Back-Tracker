8b5924dcd7a4edb370c99aaff78be65936204beb
Tyler Torola <tyler@tjt.codes>
2018-04-01 10:50:54 -0700

    Add select components

--- src/js/app/Root.jsx
+++ src/js/app/Root.jsx
@@ -1,5 +1,45 @@
 import { h } from "preact";
+import { connect } from "preact-redux";
 
-const Root = () => <div>Root</div>;
+import { setFromStation, setToStation } from "~/store/actions";
 
-export default Root;
+const Root = ({
+  fromStation,
+  setFromStation,
+  setToStation,
+  stations,
+  toStation
+}) => (
+  <div>
+    <label>From:</label>
+    <select value={fromStation} onChange={setFromStation}>
+      {stations.map(s => (
+        <option key={s.name} value={s.abbreviation}>
+          {s.name}
+        </option>
+      ))}
+    </select>
+
+    <label>To:</label>
+    <select value={toStation} onChange={setToStation}>
+      {stations.map(s => (
+        <option key={s.name} value={s.abbreviation}>
+          {s.name}
+        </option>
+      ))}
+    </select>
+  </div>
+);
+
+const mapStateToProps = state => ({
+  fromStation: state.fromStation,
+  stations: state.stations,
+  toStation: state.toStation
+});
+
+const mapDispatchToProps = dispatch => ({
+  setFromStation: e => dispatch(setFromStation(e.target.value)),
+  setToStation: e => dispatch(setToStation(e.target.value))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Root);
